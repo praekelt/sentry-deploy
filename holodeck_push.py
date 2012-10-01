d4ca8f650b49e48c41f276cd15271bf06fa51d3c
Shaun Sephton <shaun@28lines.com>
2012-10-01 13:54:07 +0200

    send all errors

--- holodeck_push.py
+++ holodeck_push.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sentry_conf")
+
 from datetime import datetime
-import random
-import time
 
 from photon import Client
+from sentry.models import Event
+from sentry.models import Project
+
 
 client = Client(
     server="http://holodeck.praekelt.com",
 )
 
+samples = []
+for project in Project.objects.all():
+    samples.append((project.name, Event.objects.filter(project=project).count()))
+
 client.send(
-    samples=(
-        ("Line 1", random.randint(100, 999)),
-        ("Line 2", random.randint(100, 999)),
-        ("Line 3", random.randint(100, 999)),
-        ("Line 4", random.randint(100, 999)),
-        ("Line 5", random.randint(100, 999)),
-    ),
+    samples=samples,
     api_key='fba3be2b97e14b6fbf32e934d757c705',
     timestamp=datetime.now(),
 )
